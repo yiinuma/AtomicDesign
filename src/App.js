0f9bea58d4f6f536bc44eb10d02a5494e7331453
yiinuma <65111001+yiinuma@users.noreply.github.com>
2021-05-07 08:08:08 +0900

    28

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./styles.css";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
 
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SearchInput />
     </div>
   );
 }
