eb3d3857845abbf49b2d0c4f6bb579a8fcbeab14
yiinuma <65111001+yiinuma@users.noreply.github.com>
2021-05-07 22:38:08 +0900

    31

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
+import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { BrowserRouter } from "react-router-dom";
 
 const user = {
   name: "じゃけぇ",
@@ -17,12 +19,14 @@ const user = {
 
 export default function App() {
   return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-    </div>
+    <BrowserRouter>
+      <DefaultLayout>
+        <PrimaryButton>テスト</PrimaryButton>
+        <SecondaryButton>検索</SecondaryButton>
+        <br />
+        <SearchInput />
+        <UserCard user={user} />
+      </DefaultLayout>
+    </BrowserRouter>
   );
 }
---
+++ src/components/templates/HeaderOnly.jsx
@@ -0,0 +1,10 @@
+import { Header } from "../atoms/layout/Header";
+export const HeaderOnly = (props) => {
+  const { children } = props;
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+};
