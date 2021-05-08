3c726ca6ec4239016019b831e7074be8afb7a032
yiinuma <65111001+yiinuma@users.noreply.github.com>
2021-05-08 15:19:26 +0900

    37

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
 import { useHistory } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../../providers/UserProvideer";
 export const Top = () => {
   const history = useHistory();
-  const onClickAdmin = () =>
-    history.push({ pathname: "/users", state: { isAdmin: true } });
-  const onClickGeneral = () =>
-    history.push({ pathname: "/users", state: { isAdmin: false } });
+  const { setUserInfo } = useContext(UserContext);
+  const onClickAdmin = () => {
+    setUserInfo({ isAdmin: true });
+    history.push("/users");
+  };
+  const onClickGeneral = () => {
+    setUserInfo({ isAdmin: false });
+    history.push("/users");
+  };
   return (
     <SContainer>
       <h2>トップページです</h2>
--- src/providers/UserProvideer.jsx
+++ src/providers/UserProvideer.jsx
@@ -1,12 +1,12 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const UserContext = createContext({});
 
 export const UserProvider = (props) => {
   const { children } = props;
-  const cotextName = "@hero";
+  const [userInfo, setUserInfo] = useState(null);
   return (
-    <UserContext.Provider value={{ cotextName }}>
+    <UserContext.Provider value={{ userInfo, setUserInfo }}>
       {children}
     </UserContext.Provider>
   );
