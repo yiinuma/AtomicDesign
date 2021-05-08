1ab1bba7cecbf9416d87bc3a7b5ce561926b1b9c
yiinuma <65111001+yiinuma@users.noreply.github.com>
2021-05-08 11:54:06 +0900

    35

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,9 +1,19 @@
 import styled from "styled-components";
-import { DefaultLayout } from "../templates/DefaultLayout";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { useHistory } from "react-router-dom";
 export const Top = () => {
+  const history = useHistory();
+  const onClickAdmin = () =>
+    history.push({ pathname: "/users", state: { isAdmin: true } });
+  const onClickGeneral = () =>
+    history.push({ pathname: "/users", state: { isAdmin: false } });
   return (
     <SContainer>
       <h2>トップページです</h2>
+      <SecondaryButton onClick={onClickAdmin}>管理者ユーザー</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onClickGeneral}>一般ユーザー</SecondaryButton>
     </SContainer>
   );
 };
